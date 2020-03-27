8b64f78f9f1c9c025610a3ec932369df39449c8c
Sol Lederer <sol.lederer@gmail.com>
2020-03-27 11:18:35 -0400

    Update ScenariosTesting.py

--- webinar/smartpy/ScenariosTesting.py
+++ webinar/smartpy/ScenariosTesting.py
@@ -1,9 +1,10 @@
 import smartpy as sp
 
 class MyContract(sp.Contract):
-    def __init__(self, myParameter1, myParameter2):
+    def __init__(self, myParameter1, myParameter2, _admin):
         self.init(myParameter1 = myParameter1,
-                  myParameter2 = myParameter2)
+                  myParameter2 = myParameter2,
+                  admin = _admin)
 
     @sp.entry_point
     def entry1(self, params):
@@ -15,35 +16,45 @@ class MyContract(sp.Contract):
     def entry2(self, params):
         sp.verify(self.data.myParameter2 - params >= 0)
         self.data.myParameter2 -= params
+        
+    @sp.entry_point
+    def reset(self):
+        sp.verify(self.data.admin <= sp.sender)
+        self.data.myParameter1 = 50
+        self.data.myParameter2 = 50
+
 
 # Tests
 @sp.add_test(name = "Test My Contract")
 def tester():
+    admin = sp.test_account("Administrator")
+    alice = sp.test_account("Alice")
+    # bob   = sp.test_account("Robert")
+    
     # We define a test scenario, together with some outputs and checks
     scenario = sp.test_scenario()
 
     # We first define a contract and add it to the scenario
-    c1 = MyContract(50, 50)
+    c1 = MyContract(50, 50, admin.address)
     scenario += c1
     
     # scenario.h1("Running through some tests")
 
-    # # And call some of its entry points
-    scenario += c1.entry1(12)
-    scenario += c1.entry1(50).run(valid = False) # this is expected to fail
-    scenario += c1.entry2(5)
-    scenario += c1.entry2(50).run(valid = False) 
+    # # # And call some of its entry points
+    # scenario += c1.entry1(12)
+    # scenario += c1.entry1(50).run(valid = False) # this is expected to fail
+    # scenario += c1.entry2(5)
+    # scenario += c1.entry2(50).run(valid = False) 
 
-    # Finally, we check its final storage
-    scenario.h4("Verifying myParameter1 = 62, and myParameter1 = 45")
-    scenario.verify(c1.data.myParameter1 == 62)
-    scenario.verify(c1.data.myParameter2 == 45)
-    
-    admin = sp.test_account("Administrator")
-    # alice = sp.test_account("Alice")
-    # bob   = sp.test_account("Robert")
+    # # Finally, we check its final storage
+    # scenario.h4("Verifying myParameter1 = 62, and myParameter1 = 45")
+    # scenario.verify(c1.data.myParameter1 == 62)
+    # scenario.verify(c1.data.myParameter2 == 45)
 
-    # scenario += c1.entry1(14).run(sender = admin, source = ..., amount = ..., now = ..., valid = ...)
-    scenario += c1.entry1(14).run(sender = admin, amount = sp.mutez(10000))
+    # scenario += c1.entry1(14).run(sender = admin, amount = sp.mutez(10000))
+    # scenario += c1.reset().run(sender = admin)
     
-    #scenario.simulation(c1)
+    # scenario.show(c1.data.myParameter1 * 12)
+    # x = scenario.compute(c1.data.myParameter1 * 12)
+    # scenario.verify(x == 500)
+    # #scenario.simulation(c1)
