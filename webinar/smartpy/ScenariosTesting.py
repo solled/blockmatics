f7d2a2ce9b9a0f6f8a675a6adfa21934ba892fc8
Sol Lederer <sol.lederer@gmail.com>
2020-03-30 13:33:36 -0400

    Update ScenariosTesting.py

--- webinar/smartpy/ScenariosTesting.py
+++ webinar/smartpy/ScenariosTesting.py
@@ -19,7 +19,7 @@ class MyContract(sp.Contract):
         
     @sp.entry_point
     def reset(self):
-        sp.verify(self.data.admin <= sp.sender)
+        sp.verify(self.data.admin == sp.sender)
         self.data.myParameter1 = 50
         self.data.myParameter2 = 50
 
@@ -29,7 +29,6 @@ class MyContract(sp.Contract):
 def tester():
     admin = sp.test_account("Administrator")
     alice = sp.test_account("Alice")
-    # bob   = sp.test_account("Robert")
     
     # We define a test scenario, together with some outputs and checks
     scenario = sp.test_scenario()
@@ -40,14 +39,14 @@ def tester():
     
     # scenario.h1("Running through some tests")
 
-    # # # And call some of its entry points
+    # # And call some of its entry points
     # scenario += c.entry1(12)
     # scenario += c.entry1(50).run(valid = False) # this is expected to fail
     # scenario += c.entry2(5)
     # scenario += c.entry2(50).run(valid = False) 
 
     # # Finally, we check its final storage
-    # scenario.h4("Verifying myParameter1 = 62, and myParameter1 = 45")
+    # scenario.h4("Verifying myParameter1 = 62, and myParameter2 = 45")
     # scenario.verify(c.data.myParameter1 == 62)
     # scenario.verify(c.data.myParameter2 == 45)
 
@@ -57,4 +56,4 @@ def tester():
     # scenario.show(c.data.myParameter1 * 12)
     # x = scenario.compute(c.data.myParameter1 * 12)
     # scenario.verify(x == 500)
-    # #scenario.simulation(c)
+    # scenario.simulation(c)
